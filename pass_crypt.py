7a4801f30a16e6fb0c96e98f14d86965786a0f66
Ricardo Dani <ricardo.dani@esharesinc.com>
2017-03-16 17:15:41 -0700

    Finished MVP

--- pass_crypt.py
+++ pass_crypt.py
@@ -1,28 +1,108 @@
-import argparse
+import os
+import sys
 from simplecrypt import encrypt, decrypt
 
-FILE_PATH = 'encpass.txt'
+class NotFoundEncryptedPass(Exception):
+    message = 'Not found with the given filename'
+
+
+class InvalidEncryptionPassword(Exception):
+    message = 'Error trying to decrypt, invalid password.'
+
+
+class InvalidPassFilename(Exception):
+    message = 'Invalid Filename (should terminate with .pass)'
+
+
+class MissingFilenameArgument(Exception):
+    message = 'Missing Filename argument.'
+
+
+class PasswordsDidntMatch(Exception):
+    message = 'Passwords don`t match.'
+
+
+class InvalidTextToEncrypt(Exception):
+    message = 'Invalid text to encrypt.'
+
+
+class NotPossibleToWrite(Exception):
+    message = 'The destiny path is not writable or the folder didn`t exists.'
+
 
 class PassCrypt(object):
     def __init__(self, path):
-        self.filepath = path
-        if self.has_file():
-            self.encrypt_password()
+        if path.endswith('.pass'):
+            self.filepath = path
         else:
-            self.create_file()
+            raise InvalidPassFilename
 
-    def has_file(self):
+    def create_encryption(self, to_encrypt, password):
         try:
-            self.file_contents = open(self.filepath, 'r')
-            return True
-        except FileNotFoundError:
-            return False
+            with open(self.filepath, 'wb') as f:
+                f.write(encrypt(password, to_encrypt))
+        except:
+            raise NotPossibleToWrite
 
-    def encrypt_password(self):
-        print('encrypt password')
+    def _decrypt(self, password, contents):
+        try:
+            return decrypt(password, contents)
+        except:
+            raise InvalidEncryptionPassword
 
-    def create_file(self):
-        print('creating file')
+    def exists(self):
+        return os.path.isfile(self.filepath)
+
+    def read_encrypted(self, password):
+        if self.exists():
+            with open(self.filepath, 'rb') as f:
+                decrypted = self._decrypt(password, f.read())
+                return decrypted.decode('utf-8')
+        raise NotFoundEncryptedPass
+
+
+class Main(object):
+
+    def __init__(self, *args):
+        try:
+            self.filename = args[0]
+        except:
+            raise MissingFilenameArgument
+        try:
+            self.run()
+        except Exception as e:
+            print (e.message)
+
+    @staticmethod
+    def read_password(confirm=False):
+        password = input('Type password: ')
+        if confirm:
+            confirm_password = input('Type password: ')
+            if password != confirm_password:
+                raise PasswordsDidntMatch
+        return password
+
+
+    @staticmethod
+    def read_contents():
+        contents = input('Type text to encrypt:\n')
+        if not contents:
+            raise InvalidTextToEncrypt
+        return contents
+
+    def run(self):
+        pc = PassCrypt(self.filename)
+        if pc.exists():
+            print(
+                pc.read_encrypted(self.read_password())
+            )
+        else:
+            pc.create_encryption(
+                self.read_contents(),
+                self.read_password(confirm=True)
+            )
+            print ('Done.')
+
 
 if __name__ == '__main__':
-    PassCrypt(FILE_PATH)
+    Main(*sys.argv[1:])
--- test_pass_crypt.py
+++ test_pass_crypt.py
@@ -1,10 +1,52 @@
+import os
 import unittest
-from pass_crypt import PassCrypt
+from pass_crypt import (
+    PassCrypt, NotFoundEncryptedPass, InvalidEncryptionPassword,
+    InvalidPassFilename
+)
 
-class TestPassCrypt(unittest.UnitTest):
+class TestPassCrypt(unittest.TestCase):
 
-    def test_create_pass_crypt(self):
-        pc = PassCrypt('test.pass')
-        pc.create_encryption('text to encrypt', 'my password')
-        with open('test.pass', 'r') as f:
-            self.assertTrue(len(f.read()) > 0)
+    TEST_FILE = 'test.pass'
+    TEST_PASSWORD = 'my password'
+
+    def setUp(self):
+        self.pc = PassCrypt(self.TEST_FILE)
+
+    def tearDown(self):
+        if os.path.isfile(self.TEST_FILE):
+            os.remove(self.TEST_FILE)
+
+    def test_filename(self):
+        self.assertEqual(self.pc.filepath, self.TEST_FILE)
+
+    def test_exception_on_invalid_filename(self):
+        with self.assertRaises(InvalidPassFilename):
+            PassCrypt('password.txt')
+
+    def test_create_pass_crypt_and_access_it(self):
+        self.pc.create_encryption('text to encrypt', self.TEST_PASSWORD)
+        with open(self.TEST_FILE, 'rb') as f:
+            contents = f.read()
+            # test if theres content
+            self.assertTrue(len(contents) > 0)
+            # test if contents is different from password
+            # (meaning that is encrypted)
+            self.assertNotEqual(contents, self.TEST_PASSWORD)
+
+        # accessing it
+        self.assertEqual(
+            self.pc.read_encrypted(self.TEST_PASSWORD),
+            b'text to encrypt'
+        )
+
+    def test_invalid_password(self):
+        self.pc.create_encryption('text to encrypt', self.TEST_PASSWORD)
+        with self.assertRaises(InvalidEncryptionPassword):
+            self.pc.read_encrypted(self.TEST_PASSWORD + ' wrong')
+
+    def test_invalid_passfile(self):
+        pc = PassCrypt('test2.pass')
+        with self.assertRaises(NotFoundEncryptedPass):
+            pc.read_encrypted('12345')
+
